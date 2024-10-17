67117d04f17950dc7b249ecae2ffe0c14efb334f
DeskThom <79005368+DeskThom@users.noreply.github.com>
2024-10-17 20:34:28 +0200

    Update and rename test_01_diabetes_predictor.py to test_01_baseball_predictor.py

--- Applications/prediction-ui-embedded/tests/test_01_baseball_predictor.py
+++ Applications/prediction-ui-embedded/tests/test_01_baseball_predictor.py
@@ -3,17 +3,17 @@ import os
 import pandas as pd
 
 # content of test_class.py
-import diabetes_predictor
+import baseball_predictor
 
 
-class TestDiabetesPredictor:
+class TestBaseballPredictor:
     def test_predict_single_record(self):
         test_dir = os.path.dirname(os.path.abspath(__file__))
         test_data_file = os.path.join(test_dir, "../testResources/prediction_request.json")
-        test_model_file = os.path.join(test_dir, "../testResources/model.h5")
+        test_model_file = os.path.join(test_dir, "../testResources/mlp_model.h5")
         with open(test_data_file) as json_file:
             data = pd.read_json(json_file)
-        dp = diabetes_predictor.DiabetesPredictor(model_file=test_model_file)
+        dp = baseball_predictor.BaseballPredictor(model_file=test_model_file)
         status = dp.predict_single_record(data)
        
         assert bool(status[0]) is not None
