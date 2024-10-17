1aab91dca4d1dbab32f070cf70d45cb841afb5e5
DeskThom <79005368+DeskThom@users.noreply.github.com>
2024-10-17 19:59:38 +0200

    Update app.py

--- Applications/V2/prediction-api/app.py
+++ Applications/V2/prediction-api/app.py
@@ -2,20 +2,20 @@ import os
 
 from flask import Flask, request
 
-from diabetes_predictor import DiabetesPredictor
+from baseball_predictor import BaseballPredictor
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/diabetes_predictor/', methods=['POST']) # path of the endpoint. Except only HTTP POST request
+@app.route('/baseball_predictor/', methods=['POST']) # path of the endpoint. Except only HTTP POST request
 def predict_str():
     # the prediction input data in the message body as a JSON payload
     prediction_inout = request.get_json()
     return dp.predict_single_record(prediction_inout)
 
 
-dp = DiabetesPredictor()
+dp = BaseballPredictor()
 # The code within this conditional block will only run the python file is executed as a
 # script. See https://realpython.com/if-name-main-python/
 if __name__ == '__main__':
